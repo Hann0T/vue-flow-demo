d173f58809f64b6039be20d86dd11375289c7c91
Hann0T <hansolivas1@gmail.com>
2022-12-02 17:07:32 -0500

    feat: TopBar component.

---
+++ src/components/TopBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div class="w-full bg-gray-700 p-4">
+        <nav class="flex gap-4 items-center text-gray-200">
+            <div class="border-r pr-4">Demo</div>
+            <div>Save IVR</div>
+            <div>Load IVR</div>
+        </nav>    
+    </div>
+</template>
