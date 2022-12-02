f0f15e53b3681d26ff7f30f46e1ad733f22b057e
Hann0T <hansolivas1@gmail.com>
2022-12-02 17:07:56 -0500

    feat: Custom Node with input.

---
+++ src/components/EditableLabelNode.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Handle, Position } from '@vue-flow/core';
+import { onMounted } from 'vue';
+onMounted(() => {
+    console.log('asdf', props.data.label);
+});
+
+const props = defineProps({
+    data: {
+        type: Object,
+    },
+});
+const emit = defineEmits(['change']);
+
+const onChange = (ev) => {
+    emit('change', ev.target.value);
+};
+</script>
+
+<template>
+  <div
+    class="flex border border-black p-2.5 rounded-[3px] bg-white justify-center items-center w-[150px]"
+  >
+    <input type="text" :value="data?.label" class="text-center text-sm w-full" @input="onChange">
+
+    <Handle id="a" type="source" :position="Position.Left" class="bg-black" />
+    <Handle id="b" type="source" :position="Position.Right" class="bg-black" />
+  </div>
+</template>
