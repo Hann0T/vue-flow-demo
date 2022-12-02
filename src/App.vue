aa38b5518172c95004998a2817174e2290345c0f
Hann0T <hansolivas1@gmail.com>
2022-12-02 17:08:43 -0500

    refactor: App vue.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,96 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue';
+import TopBar from './components/TopBar.vue';
+import EditableLabelNode from './components/EditableLabelNode.vue';
+import { VueFlow, useVueFlow } from '@vue-flow/core';
+import { MarkerType, Position } from '@vue-flow/core';
+import { Background } from '@vue-flow/additional-components'
+import { ref, nextTick, watch } from 'vue';
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const { findNode, onConnect, nodes, edges, addEdges, addNodes, viewport, project, vueFlowRef, toObject } = useVueFlow({
+    nodes: [
+        { id: '1', type: 'input', label: 'Node 1', position: { x: 5, y: 250 }, sourcePosition: Position.Right },
+        { id: '2', label: 'Node 2', position: { x: 201, y: 100 }, sourcePosition: Position.Right, targetPosition: Position.Left },
+        { id: '3', label: 'Node 3', position: { x: 201, y: 400 }, sourcePosition: Position.Right, targetPosition: Position.Left },
+        { id: '4', type: 'output', label: 'Node 4', position: { x: 400, y: 200 }, targetPosition: Position.Left },
+    ],
+    edges: [
+        { id: 'e1-3', source: '1', target: '3', type: 'step' },
+        { id: 'e1-2', source: '1', target: '2', type: 'step' },
+    ]
+})
+
+const onDragOver = (event) => {
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = 'move';
+  }
+}
+
+const onDrop = (event) => {
+    console.log('asdf', toObject());
+    const type = event.dataTransfer?.getData('application/vueflow');
+    const { left, top } = vueFlowRef.value.getBoundingClientRect();
+
+    const position = project({
+        x: event.clientX - left,
+        y: event.clientY - top,
+    });
+
+    const newNode = {
+        id: getId(),
+        type,
+        position,
+        sourcePosition: Position.Right, 
+        targetPosition: Position.Left,
+        label: `${type} node`,
+    };
+
+    addNodes([newNode]);
+
+    // align node position after drop, so it's centered to the mouse
+    nextTick(() => {
+        const node = findNode(newNode.id)
+        const stop = watch(
+          () => node.dimensions,
+          (dimensions) => {
+            if (dimensions.width > 0 && dimensions.height > 0) {
+              node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 }
+              stop()
+            }
+          },
+          { deep: true, flush: 'post' },
+        )
+    })
+};
+
+const onEditableLabelNodeChanges = (newLabel, node) => {
+    findNode(node.id).label = newLabel;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <section class="relative bg-white h-screen">
+        <TopBar class="sticky z-20 top-0 left-0" />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+        <SideBar class="fixed z-20 left-0" /> 
+
+        <div class="w-full h-full" @drop.prevent="onDrop">
+            <VueFlow 
+                auto-connect 
+                @dragover.prevent="onDragOver"
+            >
+                <template #node-label="props">
+                    <EditableLabelNode 
+                        @change="(ev) => onEditableLabelNodeChanges(ev, props)" 
+                        :data="props" 
+                    />
+                </template>
+
+                <Background pattern-color="#aaa" gap="8" />
+            </VueFlow>
+        </div>
+    </section>
+</template>
