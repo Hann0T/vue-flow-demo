2b7ab41443dcb8e57caa41f871aa8baf874930e6
Hann0T <hansolivas1@gmail.com>
2023-01-17 11:50:56 -0500

    refactor: pinia config.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import './style.css'
 import '@vue-flow/core/dist/style.css';
 import '@vue-flow/core/dist/theme-default.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
